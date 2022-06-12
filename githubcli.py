b37431d9f3e465b15ea0ba5fcbbade8478108b40
Abolanosup <104039035+Abolanosup@users.noreply.github.com>
2022-06-12 17:19:18 -0400

    Update githubcli.py

--- githubcli.py
+++ githubcli.py
@@ -40,7 +40,7 @@ class GithubCli(object):
     def __init__(self, username='',password='',my='ObiDevCu'):
         self.host = 'https://github.com/'
         self.hosting = 'https://github.com'
-        self.nexushost = 'http://gitlab.uho.edu.cu'
+        self.nexushost = 'http://gitlab.uho.edu.cu/'
         self.username = username
         self.password = password
         self.session = requests.Session()
